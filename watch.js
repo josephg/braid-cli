5220fb871ffbb8e5e5b65fbb6a0904d2ce20f7d2
Seph Gentle <me@josephg.com>
2020-11-19 15:30:30 +1100

    Added support for simple object merging patches.

--- watch.js
+++ watch.js
@@ -5,34 +5,71 @@ const chalk = require('chalk')
 const url = process.argv[2] || 'http://localhost:4040/raw/posts/bar'
 const es = new EventSource(url)
 
-// console.log('es', es)
+let verbose = true // TODO: Set me with command line flags
+let streamHeaders = {}
+let patchType // 'full-snapshot' / 'update-keys' / ...?
 
-let streamHeaders = null
-
+let isFirst = true
 let value
 
-es.onmessage = e => {
-  // console.log('e.data', e.data)
+const merge = (patchType, patch) => {
+  // console.log('merge', patchType)
+  switch (patchType) {
+    case 'full-snapshot': return patch
+    case 'update-keys': {
+      // This just merges the two objects together.
+      return {...value, ...patch}
+    }
+    default: {
+      console.error('Unknown patch type', patchType)
+      return patch
+    }
+  }
+}
 
+es.onmessage = e => {
   const message = JSON.parse(e.data)
   let {headers, version, data} = message
-  // if (version === undefined) {
-  //   version = 'unknown'
-  //   data = message
-  // }
-  if (headers != null) streamHeaders = headers
 
-  value = data
-  
   console.clear()
+
+  if (isFirst) {
+    // TODO: Lowercase all values here.
+    if (headers != null) streamHeaders = {...streamHeaders, ...headers}
+    patchType = streamHeaders['x-patch-type'] || 'full-snapshot'
+    value = data
+    isFirst = false
+  } else {
+    value = merge(patchType, data)
+  }
+  
   if (streamHeaders) {
     for (const k in streamHeaders) {
       console.log(`${chalk.yellow(k)}: ${streamHeaders[k]}`)
     }
     console.log()
   }
-  console.log(`${chalk.yellow('version')}: ${chalk.cyan(version)}`)
-  console.log(`${chalk.yellow('value')}:`)
+  if (version != null) {
+    console.log(`${chalk.cyan('version')}: ${chalk.cyan(version)}`)
+  } else {
+    console.log(`${chalk.cyan('version')}: ${chalk.red('unset')}`)
+  }
 
-  console.dir(value, {compact: false, depth: null})
+  console.log(
+    `${chalk.cyan('value')}:`,
+    inspect(value, {compact: false, depth: null, colors: process.stdout.isTTY})
+  )
+
+  if (verbose) {
+    console.log()
+
+    if (patchType !== 'full-snapshot') {
+      console.log(
+        `${chalk.cyan('last change')}:`,
+        inspect(data, {compact: false, depth: null, colors: process.stdout.isTTY})
+      )
+      console.log(`${chalk.cyan('at')}:`, new Date().toLocaleTimeString())
+    }
+  
+  }
 }
